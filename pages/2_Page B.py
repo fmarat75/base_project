1d214e5860b0bf63ccd6a2baf67544effd070220
Felix Maratier <felixmaratier@gmail.com>
2024-03-15 16:39:52 +0100

    hello

--- pages/2_Page B.py
+++ pages/2_Page B.py
@@ -1,3 +1,5 @@
 import streamlit as st
+from params import *
 
 st.markdown("""# Page B """)
+st.markdown(GOOGLE_MAPS_KEY)
---
+++ params.py
@@ -0,0 +1,5 @@
+import os
+##from dotenv import load_dotenv
+##load_dotenv()
+
+GOOGLE_MAPS_KEY = os.environ.get("GOOGLE_MAPS_KEY")
