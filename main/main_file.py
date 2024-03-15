9158eca31ab88a214f94374554f06296f4a94fac
Felix Maratier <felixmaratier@gmail.com>
2024-03-15 16:56:46 +0100

    download satelite image paris

--- main/main_file.py
+++ main/main_file.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+import requests
 
 def get_df():
 
@@ -20,3 +21,13 @@ def get_titanic():
     titanic_df = pd.read_csv(filepath)
 
     return titanic_df
+
+
+# Function to download the satellite image
+def download_satellite_image(api_key, latitude, longitude, zoom=12, size='600x600'):
+    url = f"https://maps.googleapis.com/maps/api/staticmap?center={latitude},{longitude}&zoom={zoom}&size={size}&maptype=satellite&key={api_key}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+    else:
+        raise Exception(f"Failed to download image: {response.status_code}")
